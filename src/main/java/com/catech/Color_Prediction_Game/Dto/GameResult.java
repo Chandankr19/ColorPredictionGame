e710f7d211e9b362702963c79166176aea198147
Chandankr19 <137296108+Chandankr19@users.noreply.github.com>
2024-09-23 19:56:25 +0530

    add feature: add Game result class

---
+++ src/main/java/com/catech/Color_Prediction_Game/Dto/GameResult.java
@@ -0,0 +1,14 @@
+package com.catech.Color_Prediction_Game.Dto;
+
+import lombok.Data;
+
+@Data
+public class GameResult {
+
+    private long game_id;
+
+    private String result_color;
+
+    private String game_time;
+
+}
